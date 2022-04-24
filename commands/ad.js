4ec52625e480077ed52ee675ace578c12b536721
michelle <74428837+michelle12349502039@users.noreply.github.com>
2022-04-24 12:39:34 -0400

    Update and rename urmom.js to ad.js

---
+++ commands/ad.js
@@ -0,0 +1,11 @@
+/** 
+ * @type {import('../typings').Command} 
+ */
+ module.exports = {
+  name: 'ad',
+  aliases: [],
+  description: 'sends out bop ad',
+  async run(client, message, args) {
+    message.channel.send('```ᓚᘏᗢ .˚ ˳  ៸៸ **big ol portal:** __the OG p/am hub__ ᵎᵎ\n _ _ `🕊`          join our **350+ verified** p/ams!◞\n_ _ **tips on starting out,** server running, etc.◞ `🍃`\n_ _ `🕊`\n not a p/am? come **hire** some instead!◞\n_ _ **verified paid plans** to avoid scamming◞ `🍃`\n_ _`🕊`          share raider IDs & dm advertisers◞\n\n────・・⸝⸝ https://discord.gg/portal ᵎᵎ\n────・・⸝⸝ https://discord.gg/ZdDZQSfHdr ᵎᵎ\n────・・⸝⸝ https://gph.is/g/ZPqY6pd ᵎᵎ```')
+  }
+}
